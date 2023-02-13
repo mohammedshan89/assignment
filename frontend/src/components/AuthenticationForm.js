206bcff0e0400952bc24812c6a70c0ba52bf35a0
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2023-02-13 15:40:38 +0530

    finisihed user signup and login

--- frontend/src/components/AuthenticationForm.js
+++ frontend/src/components/AuthenticationForm.js
@@ -1,41 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import axios from '../api/axios';
 
 const AuthenticationForm = () => {
+  
+ const [courseDetails, setcourseDetails] = useState([]);
+
+const [email, setEmail] = useState()
+const [name,setName] = useState()
+const [course,setCourse] = useState()
+const [password,setPassword] = useState()
+const [approved,setApproved] = useState(Boolean)
+const data = {email,name,course,password}
+
+async function getAllDetails() {
+  try {
+    const courses = await axios.get("/api/course");
+
+    setcourseDetails(courses.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+useEffect(() => {
+  getAllDetails();
+}, []);
+
+
+const handleSubmit = async(e)=>{
+  e.preventDefault()
+  try {
+    const user = await axios.post("api/user/signup", data);
+    if(user.data.message){
+     setApproved(true)
+     setCourse('')
+     setEmail("");
+     setPassword("");
+     setName('')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
   return (
     <div>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Signup your account
-            </h2>
+            {!approved ? (
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Signup your account
+              </h2>
+            ) : (
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Please wait for admin approval
+              </h2>
+            )}
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6">
             <input type="hidden" name="remember" value="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
-                <label for="email-address" className="sr-only">
-                  Email address
-                </label>
                 <input
                   id="email-address"
                   name="email"
                   type="email"
-                  autocomplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                   className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Email address"
                 />
               </div>
               <div>
-                <label for="name" className="sr-only">
-                  Name
-                </label>
                 <input
                   id="name"
                   name="name"
                   type="text"
-                  autocomplete="current-password"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm mt-3"
                   placeholder="Name"
@@ -43,16 +89,20 @@ const AuthenticationForm = () => {
               </div>
               <div className="relative">
                 <select
+                  value={course}
+                  onChange={(e) => setCourse(e.target.value)}
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm mt-3"
                   id="grid-state"
                 >
-                  <option>CourseName</option>
-                  <option>Missouri</option>
-                  <option>Texas</option>
+                  {courseDetails.map((course) => {
+                    return (
+                      <option key={course._id}>{course.coursename}</option>
+                    );
+                  })}
                 </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                   <svg
-                    class="fill-current h-4 w-4"
+                    className="fill-current h-4 w-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -61,14 +111,12 @@ const AuthenticationForm = () => {
                 </div>
               </div>
               <div>
-                <label for="password" className="sr-only">
-                  Password
-                </label>
                 <input
                   id="password"
                   name="password"
                   type="password"
-                  autocomplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm mt-3"
                   placeholder="Password"
@@ -80,27 +128,13 @@ const AuthenticationForm = () => {
 
             <div>
               <button
-                type="submit"
+                onClick={handleSubmit}
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </span>
                 Sign up
               </button>
             </div>
+            <Link to='/'>Please go to login page</Link>
           </form>
         </div>
       </div>
---
+++ frontend/src/pages/User/UserLogin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdLogin from '../../components/AdLogin'
+import UserNavbar from '../../components/UserNavbar'
+
+const UserLogin = () => {
+  return (
+    <div>
+      <UserNavbar/>
+      <AdLogin user/>
+    </div>
+  )
+}
+
+export default UserLogin
